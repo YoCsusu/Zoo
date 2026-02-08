57557b3c307e4be99ace17db5b662f4de6d8defc
Sullivan <sullivan.richard@outlook.com>
2026-02-08 11:52:45 +0100

    Update distribution URL to nebula.yocsusu.com

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./configmanager')
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 // exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://localhost:8080/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://nebula.yocsusu.com/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
